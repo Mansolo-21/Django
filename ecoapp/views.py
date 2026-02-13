3e88f68d3422d98c1bc5a0ad997d32aa42afa3eb
natalie-esuha <natalie.esuha@strathmore.edu>
2026-02-13 16:42:49 +0300

    commit

---
+++ ecoapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.template import loader
+# Create your views here.
+def index(request):
+    return render(request,template_name='index.html')
+
+def about_us(request):
+    return render(request,template_name='about_us.html')
+
+
+def contact_us(request):
+    return render(request,template_name='contact_us.html')
+
+
+def services(request):
+    return render(request,template_name='services.html')
