46a1e2bd98010054c39e6cd8893d22e705d76eb1
natalie-esuha <natalie.esuha@strathmore.edu>
2026-02-16 15:35:54 +0300

    commit

--- ecoapp/urls.py
+++ ecoapp/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from  .  import views
 urlpatterns = [
     path('', views.index, name='index'),
+    path('',views.about_us, name= 'about_us'),
+    path('',views.services, name= 'services'),
+    path('',views.contact_us, name= 'contact_us'),
+    
 ]
