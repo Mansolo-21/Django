b0d0a98ae84befbfb08a91ce74873c50ca68b07e
natalie-esuha <natalie.esuha@strathmore.edu>
2026-02-17 16:20:39 +0300

    commit

--- ecoapp/views.py
+++ ecoapp/views.py
@@ -1,16 +1,13 @@
 from django.shortcuts import render
-from django.template import loader
 # Create your views here.
 def index(request):
-    return render(request,template_name='index.html')
+    return render(request,'ecoapp/index.html')
 
 def about_us(request):
-    return render(request, 'about_us.html')
-
+    return render(request, 'ecoapp/about_us.html')
 
 def contact_us(request):
-    return render(request,template_name='contact_us.html')
-
+    return render(request,'ecoapp/contact_us.html')
 
 def services(request):
-    return render(request,template_name='services.html')
+    return render(request,'ecoapp/services.html')
