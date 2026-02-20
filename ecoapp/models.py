a55f83211cc6ef3847834176dd037202926239b0
natalie-esuha <natalie.esuha@strathmore.edu>
2026-02-20 16:25:21 +0300

    html

--- ecoapp/models.py
+++ ecoapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=30)
+    email= models.EmailField()
+    message= models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f'name:{self.name} email:{self.email} message:{self.message}'
+    
+"""from django.db import models
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"""
--- ecoapp/views.py
+++ ecoapp/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Contact
 # Create your views here.
 def index(request):
     return render(request,'ecoapp/index.html')
@@ -7,7 +8,30 @@ def about_us(request):
     return render(request, 'ecoapp/about_us.html')
 
 def contact_us(request):
-    return render(request,'ecoapp/contact_us.html')
+    if request.method == "POST":
+        Contact.objects.create(
+            name=request.POST.get("name"),
+            email=request.POST.get("email"),
+            message=request.POST.get("message"),
+        )
+        return redirect("contact")
+    
+    return render( request,'ecoapp/contact_us.html')
 
 def services(request):
     return render(request,'ecoapp/services.html')
+"""
+from django.shortcuts import render, redirect
+from .models import Contact
+
+def contact(request):
+    if request.method == "POST":
+        Contact.objects.create(
+            name=request.POST.get("name"),
+            email=request.POST.get("email"),
+            message=request.POST.get("message"),
+        )
+        return redirect("contact")
+
+    return render(request, "contact.html")
+    """
